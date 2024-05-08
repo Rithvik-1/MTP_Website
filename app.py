66aefc34567990a07c2235c7ed1d72de98cf8729
Rithvik-1 <75142278+Rithvik-1@users.noreply.github.com>
2024-05-08 15:25:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,16 +44,12 @@ absorption_coefficient = st.number_input('Absorption Coefficient', min_value=0.0
 
 if st.button('Predict'):
     prediction = predict_reduction(diameter, flow_rate, absorption_coefficient)
-    prediction_text = f'Predicted E. coli reduction: <span style="color: {"red" if prediction < 5 else "black"};">{prediction:.3f} log'
+    prediction_text = f'Predicted E. coli reduction: <span style="color: {"red" if prediction < 5 else "black"};">{prediction:.3f}</span> log'
     st.markdown(prediction_text, unsafe_allow_html=True)
 
     uv_dose_value = prediction * 2.5
     uv_dose_text = f'Predicted UV dose value: {uv_dose_value:.3f} J/cm²'
     st.success(uv_dose_text)
-
-
-
-
 
 
 
