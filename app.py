e8d4ea0ccbd3af147318947f04424754c0ed41ec
Rithvik-1 <75142278+Rithvik-1@users.noreply.github.com>
2024-05-08 15:40:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,25 +38,25 @@ st.markdown(
 st.title('UV Reactor Intensity Prediction')
 st.write('Enter the values for Diameter, Flow rate, and Absorption Coefficient to predict E. coli reduction.')
 
-diameter = st.number_input('Diameter', min_value=0.0, max_value=100.0)
-flow_rate = st.number_input('Flow rate', min_value=0.0, max_value=1000.0)
-absorption_coefficient = st.number_input('Absorption Coefficient', min_value=0.0, max_value=1000.0)
+diameter = st.number_input('Diameter', min_value=0.0, max_value=100.0, value=5.0)
+flow_rate = st.number_input('Flow rate', min_value=0.0, max_value=1000.0, value=100.0)
+absorption_coefficient = st.number_input('Absorption Coefficient', min_value=0.0, max_value=1000.0, value=100.0)
 
 if st.button('Predict'):
     prediction = predict_reduction(diameter, flow_rate, absorption_coefficient)
-    # Color the text based on the value
     if prediction < 5:
         color = "red"
+        alert_msg = " - Alert: Critical low level!"
     else:
-        color = "green"  # Green for values greater than 5
-    prediction_text = f'Predicted E. coli reduction: <span style="color: {color};">{prediction:.3f}</span> log'
+        color = "green"
+        alert_msg = " - Alert: Satisfactory level!"
+        
+    prediction_text = f'Predicted E. coli reduction: <span style="color: {color};">{prediction:.3f}</span> log {alert_msg}'
     st.markdown(prediction_text, unsafe_allow_html=True)
 
     uv_dose_value = prediction * 2.5
     uv_dose_text = f'Predicted UV dose value: {uv_dose_value:.3f} J/cm²'
     st.success(uv_dose_text)
-
-
 
 # import streamlit as st
 # import pandas as pd
